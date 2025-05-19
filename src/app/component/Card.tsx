6ec62a328b8557200391f0ae591636874b7b1851
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-20 00:09:01 +0500

    added colors

--- src/app/component/Card.tsx
+++ src/app/component/Card.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { vrCard } from './data/card'
 import Image from 'next/image'
+import { orbitron } from './fonts'
 const Card = () => {
   return (
       <div className='flex gap-6 item-center justify-center'>
           {vrCard.map((index) => (
-              <div key={index.index} className='flex'>
+              <div key={index.index} className=''>
                   <Image
                       src={index.img}
                       alt={index.img}
                       height={543}
                       width={450}
-                      ></Image>
+                  ></Image>
+                  <span className='bg-gradient-to-br from-pink-500 to-blue-500 text-transparent bg-clip-text' style={orbitron.style}>{index.heading}</span>
+                  <p>{index.description }</p>
+                  
               </div>
           ))}
       </div>
