c8db531a15c69dc04dc2b9029f4dd8564a2d6e3c
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-20 14:54:23 +0500

    added data

--- src/app/component/ProCard.tsx
+++ src/app/component/ProCard.tsx
@@ -4,6 +4,14 @@ import { orbitron, podkova, poppins } from './fonts'
 import { GiCheckMark } from "react-icons/gi";
 
 const ProCard = () => {
+    const description = [
+        { para: "Access of 150+ VR games" },
+        { para: "Access exclusive games, advanced graphics, and faster load times." },
+        { para: "Enjoy a premium suite of games for the ultimate VR immersion" },
+        { para: "Unleash your gaming potential with the Pro VR Package." },
+        { para: "Stunning visuals and smooth performance for all users." },
+        { para: "High-performance VR gaming crafted for pros and enthusiasts."}
+    ]
   return (
       <div className=' border-2 border-pink-500 p-8 rounded-[2px] rounded-bl-4xl  '>
           <div className='bg-transparent text-center flex flex-col gap-2 justify-self-start'>
@@ -12,10 +20,10 @@ const ProCard = () => {
           </div>
           <div>
               <div className='flex flex-col gap-4'>
-                  {Array.from({ length: 6 }).map((_, i) => (
-                      <div key={i} className='flex'>
+                  {description.map((index, i) => (
+                      <div key={i} className='flex items-center gap-4'>
                           <GiCheckMark  className='font-black' />
-                          <p>asssssssssssssssssssssssssssssssss</p>
+                          <p className='w-80' style={podkova.style}>{index.para }</p>
                       </div>
                       
                  ))}
