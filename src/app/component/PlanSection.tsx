e54748be5be55c2ef9e2bc8fef61bf6245dd2e8b
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-20 15:14:08 +0500

    completed proplan section

--- src/app/component/PlanSection.tsx
+++ src/app/component/PlanSection.tsx
@@ -3,13 +3,17 @@ import { orbitron, poppins, podkova } from './fonts'
 import ProPlansCard from './ProCard'
 const PlanSection = () => {
   return (
-      <div  className='min-h-screen   flex flex-col gap-28 items-center justify-center '  style={{ background: 'radial-gradient(ellipse at center, #000000, #1f2937, #6b21a8)' }} >
+      <div  className='min-h-screen p-24   flex flex-col gap-28 items-center justify-center '  style={{ background: 'radial-gradient(ellipse at center, #000000, #1f2937, #6b21a8)' }} >
           <div className='text-center space-y-3' >
               <h1 style={orbitron.style} className='text-3xl' >Level of <span  className='  bg-gradient-to-r from-pink-500 to-blue-500 text-transparent bg-clip-text'>VR Experience</span></h1>
               <p style={podkova.style} className='font-extralight '>Upgrade your VR levels with new features </p>
           </div>
           
-          <ProPlansCard/>
+          <div className='flex gap-10'>
+          <span className='mt-[120px]'><ProPlansCard /></span>
+          <span className='mb-2'><ProPlansCard /></span>
+          <span className='mt-[120px]'><ProPlansCard /></span>
+          </div>
 
       </div>
   )
