140d02059374e4babdcfe58fae32e3d3a3c92da2
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-20 16:30:25 +0500

    added types

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({text}:{text:string}) => {
   return (
     <button className="bg-gradient-to-r from-pink-500 to-blue-500 text-white px-6 py-2 rounded-tr-xl rounded-bl-xl font-medium">
             {text}
