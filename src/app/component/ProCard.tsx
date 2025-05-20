fb283d325cc6a645658035ceeb5f5fc22ee4c472
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-20 15:03:34 +0500

    build plan card completely

--- src/app/component/ProCard.tsx
+++ src/app/component/ProCard.tsx
@@ -13,22 +13,22 @@ const ProCard = () => {
         { para: "High-performance VR gaming crafted for pros and enthusiasts."}
     ]
   return (
-      <div className=' border-2 border-pink-500 p-8 rounded-[2px] rounded-bl-4xl  '>
+      <div className=' border-2 border-pink-500 p-4 rounded-[2px] rounded-bl-2xl space-y-4  '>
           <div className='bg-transparent text-center flex flex-col gap-2 justify-self-start'>
               <h1 style={orbitron.style} className='text-2xl'>Pro Plan</h1>
               <h2 style={poppins.style} className='text-2xl font-medium bg-gradient-to-r from-pink-500 to-blue-500 text-transparent bg-clip-text'>$25.99</h2>
           </div>
-          <div>
+          <div className='flex flex-col justify-center items-center gap-8'>
               <div className='flex flex-col gap-4'>
                   {description.map((index, i) => (
                       <div key={i} className='flex items-center gap-4'>
                           <GiCheckMark  className='font-black' />
-                          <p className='w-80' style={podkova.style}>{index.para }</p>
+                          <p className='w-72' style={podkova.style}>{index.para }</p>
                       </div>
                       
                  ))}
               </div>
-              <Button text="Unlock Access"></Button>
+             <span className='text-3xl '> <Button text="Unlock Access" ></Button></span>
           </div>
    </div>
   )
