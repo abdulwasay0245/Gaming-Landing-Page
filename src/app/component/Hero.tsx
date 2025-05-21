1fe0a4ec4c0d2d5a325f26a3501fbe7fff15fd2b
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-21 21:46:07 +0500

    fixed an issue

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { orbitron } from './fonts'
 import Button from './Button'
-import Image from 'next/image'
+
 
 const Hero = () => {
   return (
